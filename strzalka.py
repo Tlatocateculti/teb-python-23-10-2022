69d8ac4a6d23b805aff1d0f703cf38aa025fc812
Tlatocateculti <105506714+Tlatocateculti@users.noreply.github.com>
2022-10-23 10:29:01 +0200

    Create strzalka.py

---
+++ strzalka.py
@@ -0,0 +1,31 @@
+def strzalka(skala=1,powiel=1):
+    druk=''
+    druk="**"*2*skala+"*"*skala
+    podstawa=len(druk)
+    druk=("  "*skala + druk + "  "*skala)*powiel
+    ilosc=skala*2
+    while (ilosc>0):
+        druk=("  "*skala + "*" + " "*(podstawa-2) + "*" + "  "*skala)*powiel + "\n" + druk
+        ilosc-=1
+        #ilosc=ilosc-1
+    tmp="**"*skala + "*" + " "*(podstawa-2) + "*" + "**"*skala
+    podstawa=len(tmp)-2
+    druk=tmp + "\n" + druk
+    ilosc=1
+    while(podstawa-(ilosc*2)>0):
+        druk=" "*ilosc + "*" + " "*(podstawa-(ilosc*2)) + "*" + "\n" + druk
+        ilosc+=1
+    druk=" "*ilosc+("**" if skala%2==0 else "*") + "\n" + druk
+    """
+    tmp=" "*ilosc
+    if (skala%2==0):
+        tmp+="**"
+    else:
+        tmp+="*"
+    druk=tmp+"\n"+druk
+    
+    """
+    
+    return druk 
+   
+print(strzalka(powiel=2))
